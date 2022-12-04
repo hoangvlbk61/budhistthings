f60b733b9c861954813b755a06ff46bbf08c6194
hoangvl <hoangvlbk61@gmail.com>
2022-12-04 18:27:48 +0700

    Init blog post

--- budhistthings/tailwind.config.js
+++ budhistthings/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
@@ -7,7 +8,9 @@ module.exports = {
   theme: {
     extend: {},
   },
+  darkMode: 'class', // or 'media' or 'class'
   plugins: [
     require('@tailwindcss/line-clamp'),
   ],
+  presets: [require('./utils/tailwind-preset')],
 }
